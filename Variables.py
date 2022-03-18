fc29f2be01bd3f83183cd9694d939f501116e72a
Titus Kodomuk <tituskodomuk420@gmail.com>
2022-03-18 11:23:26 +0800

    Day 2

--- Variables.py
+++ Variables.py
@@ -1,5 +1,13 @@
+#This is not entirely about variables...just random python codeing
+Trial= 'This is just a test'
+print(Trial[0:-1])
+print(len(Trial))
+print(Trial.upper())
+print(Trial.find('just'))
+print('test' in Trial)
+#operator precedence - order of operation
+x=29.6
+print(round(x))
 Name = input("What is your name son? ")
 Colour = input("What is your favourite colour? ")
 print(Name + " likes " +Colour)
-
-

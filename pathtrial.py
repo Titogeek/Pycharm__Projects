d4417a1e07fa2afa3c4ad7faaf1a2e69ebce79b5
Titus Kodomuk <tituskodomuk420@gmail.com>
2022-10-26 10:38:44 +0300

    Daily Updates

--- pathtrial.py
+++ pathtrial.py
@@ -1,9 +1,14 @@
 from pathlib import Path
 
+path3 = Path()
+
+for file in path3.glob('*.py'):
+    print(file)
+
 path = Path("maths")
 print(path.exists)
 # creating a new directory in project file
-path2 = Path("Trialdir1")
+path2 = Path("Trialdir2")
 path2.mkdir()
 # Deleting a existing directory
 # path2.rmdir()
